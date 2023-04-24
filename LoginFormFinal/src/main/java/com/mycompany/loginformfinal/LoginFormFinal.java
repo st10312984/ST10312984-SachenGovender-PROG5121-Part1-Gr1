2a57dd5ab0ab8c3339576139e4eee057f7bb5ac7
Sachen Govender <128683098+st10312984@users.noreply.github.com>
2023-04-24 13:43:18 +0000

    Add files via upload

---
+++ LoginFormFinal/src/main/java/com/mycompany/loginformfinal/LoginFormFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.loginformfinal;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class LoginFormFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
